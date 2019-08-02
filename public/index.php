7f35362da3b6df5030e0b1cdbae5a0c812d6f9a4
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-08-02 14:01:19 +0530

    Slider and Navs Snippet Creation

--- public/index.php
+++ public/index.php
@@ -4,45 +4,19 @@
     <div class="container">
 
         <div class="row">
-
-            <div class="col-md-3">
-                <p class="lead">Shop Name</p>
-                <div class="list-group">
-                    <a href="category.html" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
-                </div>
-            </div>
+            <!-- categories goes here [sidenav] -->
+			<?php require_once TEMPLATE_FRONT.DS."sidenav.php"; ?>
+            <!-- categories goes here [sidenav] -->
+          
 
             <div class="col-md-9">
 
                 <div class="row carousel-holder">
 
                     <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
+                        <!-- CAROSAL -->
+						<?php require_once TEMPLATE_FRONT.DS."slider.php"; ?>
+                        <!-- CAROSAL -->
                     </div>
 
                 </div>
