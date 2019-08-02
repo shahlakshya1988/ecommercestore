c57da3d73eeecbaf258b8db2ed45bf8e6ef168e8
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-08-02 14:28:47 +0530

    Displaying Dynamic Categories

--- resources/templates/front/sidenav.php
+++ resources/templates/front/sidenav.php
@@ -1,8 +1,17 @@
   <div class="col-md-3">
                 <p class="lead">Shop Name</p>
                 <div class="list-group">
-                    <a href="category.html" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
+				<?php
+					$select_category_query="SELECT * FROM `categories`";
+					$select_category = $db->prepare($select_category_query);
+					if($select_category->execute()){
+						while($row_category = $select_category->fetch(PDO::FETCH_OBJ)){
+							echo "<a href=\"category.html\" class=\"list-group-item\">{$row_category->cat_title}</a>";
+						}
+					} //if($select_category->execute())
+				?>
+                    
+                    <?php /* <a href="#" class="list-group-item">Category 2</a>
+                    <a href="#" class="list-group-item">Category 3</a> */ ?>
                 </div>
             </div>
