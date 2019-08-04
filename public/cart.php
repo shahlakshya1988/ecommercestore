ef98db1d1e1c777eba0b53907cbd40062cac6815
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-08-04 15:28:53 +0530

    Add Product Session Code Part 2

--- public/cart.php
+++ public/cart.php
@@ -1,10 +1,20 @@
 <?php 
 require_once "../resources/config.php"; 
 if(isset($_GET["add"])){
-    $add = trim($_GET["add"]);
-    $_SESSION["product_".$add]++;
-    redirect("index.php");
-    die();
+    $product_id = trim($_GET["add"]);
+    $sel_product_query = "SELECT `product_quantity` FROM `products` where `product_id` = :product_id";
+    $sel_product = $db->prepare($sel_product_query);
+    $sel_product->execute(array(":product_id"=>$product_id));
+    $result = $sel_product->fetch(PDO::FETCH_OBJ);
+   //var_dump($result->product_quantity);
+   if($result->product_quantity < $_SESSION["product_".$product_id]){
+        $_SESSION["product_".$product_id]++;
+   }else{
+        setMessage("We Only Have {$result->product_quantity} Available");
+        
+   }
+   redirect("checkout.php");
+    
 }
 
 ?>
